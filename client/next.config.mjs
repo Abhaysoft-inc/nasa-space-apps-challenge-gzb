7d4ecbd2c20e1fcb17bbf41c669bb042304f057e
Ishaan Pandey <ishaanmaybe@gmail.com>
2025-09-28 09:36:08 +0530

    Add NASA rovers image to the public directory

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{ protocol: 'https', hostname: 'images.unsplash.com' },
+			{ protocol: 'https', hostname: 'source.unsplash.com' },
+		],
+	},
+}
